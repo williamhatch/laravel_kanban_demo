70861d528a27b0064cde3e57135b2ad0b5204ecf
williamhatch <trees.william@gmail.com>
2025-06-22 10:58:16 +1200

    fix(testing): Configure in-memory database and resolve risky test

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -7,7 +7,8 @@ use PHPUnit\Framework\TestCase;
 class ExampleTest extends TestCase
 {
     /**
-     * A basic test example.
+     * A basic unit test example.
+     * @coversNothing
      */
     public function test_that_true_is_true(): void
     {
